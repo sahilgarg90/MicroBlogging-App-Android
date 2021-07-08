f3ba7aaa74acc1c2e101c0e55b2c37c2a085d3c4
Sahil Garg <sahil_garg@hotmail.com>
2021-07-08 10:53:24 +0530

    Created New Project

---
+++ app/src/main/java/com/sahilgarg90/microblogging/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.sahilgarg90.microblogging.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
